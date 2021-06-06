97cb5d0a9f3b959d55668498f5749eb3cedee6e8
soundarya-sketch <76419003+soundarya-sketch@users.noreply.github.com>
2021-06-06 23:59:13 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>I loved being the part of this bootcamp 
+         looking forward for more useful bootcamps like this
+      </p>
+    </div>
+  );
+}
+
+export default Note;
